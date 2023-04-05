05ecf5ab0932c5468ca57e866e56967b94310131
Avinash <avjain@qodeleaf.com>
2023-04-05 18:21:56 +0530

    Update hardhat.config.js to use credentials from dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 require("solidity-coverage");
 require("hardhat-deploy");
 require("@nomiclabs/hardhat-ethers");
-
+require("dotenv").config();
+const { INFURA_API_KEY, SEPOLIA_PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const INFURA_API_KEY = "0b56c8e153e6499d8f9d567092ec32fe";
-const SEPOLIA_PRIVATE_KEY =
-  "0x9d8276f0e5585e1cd5fbdf047cebac1ce86daf6ee03998d06d21f427a241d371";
 module.exports = {
   solidity: "0.8.18",
   nameAccounts: {
