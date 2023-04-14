d774380251cece9babe82a62992fb2857ae6144b
Avinash <avjain@qodeleaf.com>
2023-04-14 16:01:54 +0530

    update LiquidityLocker.js:    update: add tests for update in LiquidityLocker.sol

--- test/LiquidityLocker.js
+++ test/LiquidityLocker.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
-describe("Liquidity Locker Contract", function () {
+describe("Tests for Liquidity Locker Contract", function () {
   async function deployTokenFixture() {
     const ERC20Token = await ethers.getContractFactory("ERC20Token");
     const LiquidityLocker = await ethers.getContractFactory("LiquidityLocker");
@@ -28,110 +28,113 @@ describe("Liquidity Locker Contract", function () {
       zero_address,
     };
   }
-  describe("scheduleVesting function", function () {
-    it("only owner can call this function", async function () {
+  describe("Tests for scheduleVesting function", function () {
+    it("Only contract owner can schedule a vest.", async function () {
       const { hardhatLiquidityLocker, user1 } = await loadFixture(
         deployTokenFixture
       );
       await expect(
-        hardhatLiquidityLocker.connect(user1).scheduleVesting(30, 10, 10, 10)
-      ).to.be.revertedWith("LiquidityLocker: Only owner can Schedule Vesting");
+        hardhatLiquidityLocker.connect(user1).scheduleVesting(30, 10, 10)
+      ).to.be.revertedWith(
+        "LiquidityLocker: Only owner can Schedule Vesting and approve referer."
+      );
     });
-    it("Cliff duration not greater than end Timestamp", async function () {
+    it("Vest's cliff duration should not be greater than its end Timestamp.", async function () {
       const { hardhatLiquidityLocker, owner } = await loadFixture(
         deployTokenFixture
       );
       await expect(
-        hardhatLiquidityLocker.scheduleVesting(10, 30, 10, 10)
+        hardhatLiquidityLocker.scheduleVesting(10, 30, 10)
       ).to.be.revertedWith(
         "LiquidityLocker: Cliff Duration cannot be greater than End timestamp"
       );
     });
 
-    it("should emit VestingScheduled event", async function () {
+    it("On completing of scheduleVesting(), it should emit a VestingScheduled event.", async function () {
       const { hardhatLiquidityLocker } = await loadFixture(deployTokenFixture);
-      await expect(hardhatLiquidityLocker.scheduleVesting(30, 10, 10, 10))
+      await expect(hardhatLiquidityLocker.scheduleVesting(30, 10, 10))
         .to.emit(hardhatLiquidityLocker, "VestingScheduled")
         .withArgs(1, 30, 10);
     });
   });
-  describe("vestAssests function", function () {
-    it("schedule does not exist", async function () {
+  describe("Tests for vestAssests function", function () {
+    it("On adding assests to a vest, that vest should have scheduled by owner.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker, owner, user1 } =
         await loadFixture(deployTokenFixture);
       await expect(
         hardhatLiquidityLocker.connect(user1).vestAssests(1000, 1)
       ).to.be.revertedWith("LiquidityLocker: This schedule does not exist");
     });
-    it("already a benefitter", async function () {
+    it("Existing benefiter cannot create a new vest.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1000);
       await hardhatLiquidityLocker.vestAssests(500, 1);
       expect(hardhatLiquidityLocker.vestAssests(500, 1)).to.be.revertedWith(
         "LiquidityLocker: sender already have assests vested in contract."
       );
     });
-    it("only non zero assests can be vested", async function () {
+    it("User should add non zero assests to vest.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1000);
       await expect(hardhatLiquidityLocker.vestAssests(0, 1)).to.be.revertedWith(
         "LiquidityLocker: No of token should be greater than zero."
       );
     });
-    it("Vesting by a refered address", async function () {
+    it("The user is referd by a approved referer.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker, user1, owner } =
         await loadFixture(deployTokenFixture);
+      await hardhatLiquidityLocker.addReferer(user1.address);
       await hardhatLiquidityLocker.connect(user1).refer(owner.address);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatERC20Token.transfer(hardhatLiquidityLocker.address, 100);
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1000);
       await hardhatLiquidityLocker.vestAssests(1000, 1);
       expect(await hardhatERC20Token.balanceOf(user1.address)).to.equal(7);
     });
   });
-  describe("withdraw Function", function () {
-    it("only benefiter can withdraw assests", async function () {
+  describe("Tests for withdraw Function", function () {
+    it("Users with existing vests only can withdraw assets.", async function () {
       const { hardhatLiquidityLocker } = await loadFixture(deployTokenFixture);
       await expect(
         hardhatLiquidityLocker.withdrawToken(100)
       ).to.be.revertedWith(
-        "LiquidityLocker: only benefiters can withdraw or add vested token"
+        "LiquidityLocker: only benefiters can withdraw or add token from/to vest"
       );
     });
-    it("Not enough token to withdraw", async function () {
+    it("Number of assests to withdraw should not be greater than assests in vest.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 100);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatLiquidityLocker.vestAssests(100, 1);
       await expect(
         hardhatLiquidityLocker.withdrawToken(150)
       ).to.be.revertedWith("LiquidityLocker: Not enough token vested");
     });
-    it("Cliff duration have not yet passed", async function () {
+    it("To withdraw assests from vest. cliff duration must have elapsed.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 100);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatLiquidityLocker.vestAssests(100, 1);
       await expect(hardhatLiquidityLocker.withdrawToken(50)).to.be.revertedWith(
         "LiquidityLocker: cliff duration have not passed"
       );
     });
-    it("Successful withdrawal", async function () {
+    it("On successful withdrawal of assests from vest.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 100);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatLiquidityLocker.vestAssests(100, 1);
       await helpers.time.increase(40);
       await hardhatLiquidityLocker.withdrawToken(50);
@@ -140,31 +143,31 @@ describe("Liquidity Locker Contract", function () {
       ).to.equal(50);
     });
   });
-  describe("dropAllAssests", function () {
-    it("Only user with vested Assessts can drop", async function () {
+  describe("Tests for dropAllAssests function", function () {
+    it("Only user with existing vest can drop there vest.", async function () {
       const { hardhatLiquidityLocker } = await loadFixture(deployTokenFixture);
       await expect(hardhatLiquidityLocker.dropAllAssests()).to.be.revertedWith(
-        "LiquidityLocker: only benefiters can withdraw or add vested token"
+        "LiquidityLocker: only benefiters can withdraw or add token from/to vest"
       );
     });
-    it("Vested assests have not reached maturity", async function () {
+    it("User vest should have matured with dropping.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 100);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatLiquidityLocker.vestAssests(100, 1);
       await expect(hardhatLiquidityLocker.dropAllAssests()).to.be.revertedWith(
         "LiquidityLocker: Vested assests have not reached their maturity."
       );
     });
-    it("Successfully dropped all assests", async function () {
+    it("On successful dropping of vest.", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1000);
       await hardhatERC20Token.transfer(hardhatLiquidityLocker.address, 100);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatLiquidityLocker.vestAssests(1000, 1);
       await helpers.time.increase(60);
       await hardhatLiquidityLocker.dropAllAssests();
@@ -173,8 +176,16 @@ describe("Liquidity Locker Contract", function () {
       ).to.equal(33);
     });
   });
-  describe("refer function", function () {
-    it("Invalid address to refer", async function () {
+  describe("Tests for refer function", function () {
+    it("Only address approved by owner can refer.", async function () {
+      const { hardhatLiquidityLocker, user1 } = await loadFixture(
+        deployTokenFixture
+      );
+      await expect(
+        hardhatLiquidityLocker.refer(user1.address)
+      ).to.be.revertedWith("LiquiityLocker: Only approved referer can refer");
+    });
+    it("Referer cannot refer a invalid address for referal reward.", async function () {
       const { hardhatLiquidityLocker, zero_address } = await loadFixture(
         deployTokenFixture
       );
@@ -182,10 +193,11 @@ describe("Liquidity Locker Contract", function () {
         hardhatLiquidityLocker.refer(zero_address)
       ).to.be.revertedWith("LiquidityLocker: Invalid address");
     });
-    it("Address already refered", async function () {
-      const { hardhatLiquidityLocker, user1 } = await loadFixture(
+    it("Referer cannnot refer already refered address for referal reward.", async function () {
+      const { hardhatLiquidityLocker, user1, owner } = await loadFixture(
         deployTokenFixture
       );
+      await hardhatLiquidityLocker.addReferer(owner.address);
       await hardhatLiquidityLocker.refer(user1.address);
       await expect(
         hardhatLiquidityLocker.refer(user1.address)
@@ -193,21 +205,24 @@ describe("Liquidity Locker Contract", function () {
         "LiquidityLocker: This user address is already refered."
       );
     });
-    it("Address cannot refer himself", async function () {
+    it("Referer cannot refer himself.", async function () {
       const { hardhatLiquidityLocker, owner } = await loadFixture(
         deployTokenFixture
       );
-
+      await hardhatLiquidityLocker.addReferer(owner.address);
       await expect(
         hardhatLiquidityLocker.refer(owner.address)
       ).to.be.revertedWith(
         "LiquidityLocker: Msg Sender cannot approve himself."
       );
     });
-    it("Two address cannot refer one another", async function () {
+    it("Two referer address cannot refer one another", async function () {
       const { hardhatLiquidityLocker, owner, user1 } = await loadFixture(
         deployTokenFixture
       );
+      await hardhatLiquidityLocker.addReferer(owner.address);
+      await hardhatLiquidityLocker.addReferer(user1.address);
+
       await hardhatLiquidityLocker.refer(user1.address);
       await expect(
         hardhatLiquidityLocker.connect(user1).refer(owner.address)
@@ -216,57 +231,66 @@ describe("Liquidity Locker Contract", function () {
       );
     });
   });
+  describe("addReferer function", function () {
+    it("Only owner can approve address to refer", async function () {
+      const { hardhatLiquidityLocker, user1, owner } = await loadFixture(
+        deployTokenFixture
+      );
+      await expect(
+        hardhatLiquidityLocker.connect(user1).addReferer(owner.address)
+      ).to.be.revertedWith(
+        "LiquidityLocker: Only owner can Schedule Vesting and approve referer."
+      );
+    });
+    it("Referer cannot refer zero address user for refer reward.", async function () {
+      const { hardhatLiquidityLocker, owner, zero_address } = await loadFixture(
+        deployTokenFixture
+      );
+      await expect(
+        hardhatLiquidityLocker.addReferer(zero_address)
+      ).to.be.revertedWith("LiquidityLocker: Invalid address");
+    });
+  });
   describe("addAssests function", function () {
     it("Only user with vested Assessts can Add", async function () {
       const { hardhatLiquidityLocker } = await loadFixture(deployTokenFixture);
       await expect(hardhatLiquidityLocker.addAssests(100)).to.be.revertedWith(
-        "LiquidityLocker: only benefiters can withdraw or add vested token"
+        "LiquidityLocker: only benefiters can withdraw or add token from/to vest"
       );
     });
-    it("Need atleast 20% of vested of assests to be added", async function () {
+    it("Benefiter cannot add zero assests in existing vest", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1000);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatLiquidityLocker.vestAssests(1000, 1);
-      await expect(hardhatLiquidityLocker.addAssests(100)).to.be.revertedWith(
-        "LiquidityLocker: add atleast 20% of already vested assests"
+      await expect(hardhatLiquidityLocker.addAssests(0)).to.be.revertedWith(
+        "LiquidityLocker: Enter non zero tokens to add."
       );
     });
-    it("cannot add assests after cliff duration", async function () {
+
+    it("Adding assests after maturity time have elapsed", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
-      await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1000);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1100);
+      await hardhatLiquidityLocker.scheduleVesting(30, 10, 10);
       await hardhatLiquidityLocker.vestAssests(1000, 1);
-      await helpers.time.increase(60);
-      await expect(hardhatLiquidityLocker.addAssests(300)).to.be.revertedWith(
-        "LiquidityLocker: cannot add assest after cliffDuration."
-      );
+      await helpers.time.increase(30);
+      await hardhatLiquidityLocker.addAssests(100);
+      await expect(
+        await hardhatERC20Token.balanceOf(hardhatLiquidityLocker.address)
+      ).to.be.equal(1100);
     });
-    it("consecutive addAssests() should have a proper time interval in between", async function () {
-      const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
-        deployTokenFixture
-      );
-      await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1000);
-      // await hardhatERC20Token.transfer(hardhatLiquidityLocker.address, 100);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
-      await hardhatLiquidityLocker.vestAssests(1000, 1);
-      // await helpers.time.increase(60);
-      await expect(hardhatLiquidityLocker.addAssests(300)).to.be.revertedWith(
-        "LiquidityLocker: Duration between adding assests have not elapsed. "
-      );
-    });
-    it("Succesful in adding Assests", async function () {
+
+    it("Succesful addition in vested assests", async function () {
       const { hardhatERC20Token, hardhatLiquidityLocker } = await loadFixture(
         deployTokenFixture
       );
       await hardhatERC20Token.approve(hardhatLiquidityLocker.address, 1300);
-      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10, 10);
+      await hardhatLiquidityLocker.scheduleVesting(60, 30, 10);
       await hardhatLiquidityLocker.vestAssests(1000, 1);
-      await helpers.time.increase(10);
       await hardhatLiquidityLocker.addAssests(300);
       await expect(
         await hardhatERC20Token.balanceOf(hardhatLiquidityLocker.address)
